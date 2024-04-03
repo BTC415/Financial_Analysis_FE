1de4ad1589994f6fe42035e546bbacbd8f374f9c
renebeahan <renebeahan124@gmail.com>
2024-04-03 15:13:28 +0100

    detail

--- components/Detail.tsx
+++ components/Detail.tsx
@@ -17,45 +17,45 @@ const Detail: React.FC<DetailProps> = ({ transaction, clicked, setClicked }) => 
         <div className='w-full bg-gray-500 opacity-50 h-full fixed left-0 right-0 top-0 bottom-0 ' onClick={() => setClicked(false)}></div>
         <div className='bg-white md:w-[30%] fixed right-0 md:top-0 mt-[72px] md:h-full shadow-md z-50 duration-300 ease-in-out bottom-0 min-h-fit w-full '>
           <div className='p-8 flex justify-between items-center border-b-2 border-gray-300'>
-            <p className='font-bold sm:text-[4rem] text-[2rem]'>Transação</p>
+            <p className='font-bold lg:text-[4rem] md:text-[4.1rem] sm:text-[4.1rem] text-[2.9rem]'>Transação</p>
             <button onClick={() => setClicked(false)}>
               <svg width={20} height={20}><use href='#svg-exit' /></svg>
             </button>
           </div>
           <div className='my-4 p-4 '>
             <div className='p-3 flex justify-between items-center'>
-              <p className='sm:text-[3rem] text-[2rem]'>Número</p>
-              <p className='sm:text-[3rem] text-[2rem]'>{transaction?.id}</p>
+              <p className='lg:text-[3.5rem] md:text-[3.1rem] sm:text-[3.1rem] text-[2.2rem]'>Número</p>
+              <p className='lg:text-[3.5rem] md:text-[3.1rem] sm:text-[3.1rem] text-[2.2rem]'>{transaction?.id}</p>
             </div>
-            <div className='p-3 flex justify-between items-center text-[2rem]'>
-              <p className='sm:text-[3rem] text-[2rem]'>Data</p>
+            <div className='p-3 flex justify-between items-center lg:text-[3rem] md:text-[2.7rem] sm:text-[2.7rem] text-[2rem]'>
+              <p className='lg:text-[3.5rem] md:text-[3.1rem] sm:text-[3.1rem] text-[2.2rem]'>Data</p>
               { transaction && <DetailedDate dateString={transaction.Data} time={transaction.time} /> }
             </div>
             <div className='p-3 flex justify-between items-center'>
-              <p className='sm:text-[3rem] text-[2rem]'>Prestador</p>
+              <p className='lg:text-[3.5rem] md:text-[3.1rem] sm:text-[3.1rem] text-[2.2rem]'>Prestador</p>
               <p className='col-span-2 sm:text-[2rem] text-[1.5rem] p-2 flex items-center'>
                 {transaction?.Tipo === 'Retirada' ? (
                   <div className='flex flex-row gap-2 items-center'>
                     <Image src="/avatar2.png" alt="avatar2" width={24} height={24} />
-                    <p className='text-[2rem]'>{transaction.Prestador}</p>
+                    <p className='lg:text-[3.5rem] md:text-[3.1rem] sm:text-[3.1rem] text-[2.2rem]'>{transaction.Prestador}</p>
                   </div>
                 ) : null}
               </p>
             </div>
             <div className='p-3 flex justify-between items-center border-b-2 border-gray-300'>
-              <p className='sm:text-[3rem] text-[2rem]'>Tipo</p>
-              <p className={`col-span-1 sm:text-[2rem] text-[1.5rem] my-1 p-1 rounded-full px-4 ${transaction?.Tipo === 'Retirada' ? 'bg-red-300 text-red-600' : 'bg-green-300 text-green-600'}`}>{transaction?.Tipo}</p>
+              <p className='lg:text-[3.5rem] md:text-[3.1rem] sm:text-[3.1rem] text-[2.2rem]'>Tipo</p>
+              <p className={`col-span-1 lg:text-[3rem] md:text-[2.7rem] sm:text-[2.7rem] text-[2rem] my-1 p-1 rounded-full px-4 ${transaction?.Tipo === 'Retirada' ? 'bg-red-300 text-red-600' : 'bg-green-300 text-green-600'}`}>{transaction?.Tipo}</p>
             </div>
           </div>
           <div>
-            <p className='m-4 p-4 font-bold sm:text-[3rem] text-[2rem]'>Detalhes da transação</p>
+            <p className='m-4 p-4 font-bold lg:text-[4rem] md:text-[4.1rem] sm:text-[4.1rem] text-[2.9rem]'>Detalhes da transação</p>
             <div className='px-8 py-2 flex justify-between items-center'>
-              <p className='sm:text-[3rem] text-[2rem]'>Valor</p>
-              <p className='sm:text-[3rem] text-[2rem]'>{`R$ ${transaction?.Valor},00`}</p>
+              <p className='lg:text-[3.5rem] md:text-[3.1rem] sm:text-[3.1rem] text-[2.2rem]'>Valor</p>
+              <p className='lg:text-[3.5rem] md:text-[3.1rem] sm:text-[3.1rem] text-[2.2rem]'>{`R$ ${transaction?.Valor},00`}</p>
             </div>
             <div className='px-8 py-2 flex justify-between items-center'>
-              <p className='sm:text-[3rem] text-[2rem]'>% Catajobs</p>
-              <p className='sm:text-[3rem] text-[2rem]'><span className='sm:text-[2rem] text-[1.5rem]'>5%</span> {`R$ 5,00`}</p>
+              <p className='lg:text-[3.5rem] md:text-[3.1rem] sm:text-[3.1rem] text-[2.2rem]'>% Catajobs</p>
+              <p className='lg:text-[3.5rem] md:text-[3.1rem] sm:text-[3.1rem] text-[2.2rem]'><span className='lg:text-[3rem] md:text-[2.7rem] sm:text-[2.7rem] text-[2rem]'>5%</span> {`R$ 5,00`}</p>
             </div>
           </div>
         </div>
