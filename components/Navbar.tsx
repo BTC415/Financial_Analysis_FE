380f2e784cdd5242c986a4a1a9f2507cd92522b5
Mit0213 <mitsurukudo213@gmail.com>
2024-08-19 12:01:37 -0500

    Modified Navbar component style

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <>
+    <div>
       <div className="hidden sm:block lg:px-[8.2rem] md:px-[5.26rem] sm:px-[3.94rem] px-[2.37rem] bg-white">
         <div className="flex justify-between items-center sm:w-full h-[7.03vh] ">
           <div>
@@ -11,6 +11,7 @@ const Navbar = () => {
             </svg>
           </div>
           <div className="flex flex-row gap-[1.5rem] justify-between items-center">
+
             <div className="bg-[#F3F3F5] flex justify-around flex-row items-center lg:w-[17vw] h-[63.89%] md:w-[25vw] sm:w-[30vw] rounded-[1rem]">
               <svg width={24} height={24}>
                 <use href="#svg-wallet" />
@@ -26,11 +27,13 @@ const Navbar = () => {
                 <p className="text-white text-[12px]">Depositar</p>
               </div>
             </div>
+
             <div>
               <svg width={24} height={40}>
                 <use href="#svg-vertical-bar" />
               </svg>
             </div>
+
             <div className="flex flex-row lg:w-[11.53vw] md:w-[17vw] sm:w-[23vw] items-center justify-around">
               <Image src="/avatar.png" alt="avatar" width={40}
                 height={40}
@@ -42,6 +45,7 @@ const Navbar = () => {
                 <use href="#svg-down-arrow" />
               </svg>
             </div>
+
           </div>
         </div>
       </div>
@@ -69,20 +73,20 @@ const Navbar = () => {
           </div>
           <div className="flex flex-row gap-[1.5rem] p-[1.5rem] items-center">
 
-            <Image src="/avatar.png" alt="avatar"  width={32}
-                height={32} sizes="2.5rem"
-                style={{
-                  width: '100%',
-                  height: 'auto',
-                }} className="rounded-full" />
-            
+            <Image src="/avatar.png" alt="avatar" width={32}
+              height={32} sizes="2.5rem"
+              style={{
+                width: '100%',
+                height: 'auto',
+              }} className="rounded-full" />
+
             <svg width={40} height={40}>
               <use href="#svg-three-bar-decreased" />
             </svg>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
